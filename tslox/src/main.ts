be89b80d52de514be3c2c2781466cd497cf70cb2
Daniel McLaughlin <danielsmc@gmail.com>
2024-09-17 22:17:46 -0400

    Initial setup + scanning

---
+++ tslox/src/main.ts
@@ -0,0 +1,45 @@
+import { scan } from "./scan.ts";
+import { tokenToString } from "./Token.ts";
+
+let hadError = false;
+
+const [file, ...extras] = Deno.args;
+if (extras.length) {
+  console.log("Usage: tslox [script]");
+  Deno.exit(64);
+} else if (file) {
+  runFile(file);
+} else {
+  runPrompt();
+}
+
+function runFile(file: string) {
+  run(Deno.readTextFileSync(file));
+  if (hadError) Deno.exit(65);
+}
+
+function runPrompt() {
+  while (true) {
+    const line = prompt("> ");
+    if (line === null) break;
+    run(line);
+    hadError = false;
+  }
+}
+
+function run(source: string) {
+  const tokens = scan(source);
+
+  for (const token of tokens) {
+    console.log(tokenToString(token));
+  }
+}
+
+export function loxError(line: number, message: string) {
+  report(line, "", message);
+}
+
+function report(line: number, where: string, message: string) {
+  console.log(`[line ${line}] Error ${where}: ${message}`);
+  hadError = true;
+}
---
+++ tslox/src/scan.ts
@@ -0,0 +1,197 @@
+import { Token, TokenType } from "./Token.ts";
+import { loxError } from "./main.ts";
+
+const keywords = Object.fromEntries(
+    ([
+        "AND",
+        "CLASS",
+        "ELSE",
+        "FALSE",
+        "FUN",
+        "FOR",
+        "IF",
+        "NIL",
+        "OR",
+        "PRINT",
+        "RETURN",
+        "SUPER",
+        "THIS",
+        "TRUE",
+        "VAR",
+        "WHILE",
+    ] satisfies TokenType[]).map((k) => [k.toLowerCase(), k]),
+);
+
+export function scan(source: string): Token[] {
+    const tokens: Token[] = [];
+    let start = 0;
+    let current = 0;
+    let line = 1;
+
+    while (!isAtEnd()) {
+        start = current;
+        scanToken();
+    }
+    tokens.push({
+        type: "EOF",
+        lexeme: "",
+        line,
+    });
+
+    return tokens;
+
+    function scanToken() {
+        const c = advance();
+        switch (c) {
+            case "(":
+                addToken("LEFT_PAREN");
+                break;
+            case ")":
+                addToken("RIGHT_PAREN");
+                break;
+            case "{":
+                addToken("LEFT_BRACE");
+                break;
+            case "}":
+                addToken("RIGHT_BRACE");
+                break;
+            case ",":
+                addToken("COMMA");
+                break;
+            case ".":
+                addToken("DOT");
+                break;
+            case "-":
+                addToken("MINUS");
+                break;
+            case "+":
+                addToken("PLUS");
+                break;
+            case ";":
+                addToken("SEMICOLON");
+                break;
+            case "*":
+                addToken("STAR");
+                break;
+            case "!":
+                addToken(match("=") ? "BANG_EQUAL" : "BANG");
+                break;
+            case "=":
+                addToken(match("=") ? "EQUAL_EQUAL" : "EQUAL");
+                break;
+            case "<":
+                addToken(match("=") ? "LESS_EQUAL" : "LESS");
+                break;
+            case ">":
+                addToken(match("=") ? "GREATER_EQUAL" : "GREATER");
+                break;
+            case "/":
+                if (match("/")) {
+                    // comment
+                    while (peek() !== "\n" && !isAtEnd) advance();
+                } else {
+                    addToken("SLASH");
+                }
+                break;
+            case " ":
+            case "\r":
+            case "\t":
+                break;
+            case "\n":
+                line++;
+                break;
+            case '"':
+                string();
+                break;
+            default:
+                if (isDigit(c)) {
+                    number();
+                } else if (isAlpha(c)) {
+                    identifier();
+                } else {
+                    loxError(line, "Unexpected character.");
+                }
+        }
+    }
+
+    function identifier() {
+        while (isAlphaNumeric(peek())) advance();
+        const type: TokenType = keywords[curLexeme()] ?? "IDENTIFIER";
+        addToken(type);
+    }
+
+    function number() {
+        while (isDigit(peek())) advance();
+
+        if (peek() == "." && isDigit(peekNext())) {
+            // consume the decimal point
+            advance();
+
+            while (isDigit(peek())) advance();
+        }
+
+        addToken("NUMBER", parseFloat(curLexeme()));
+    }
+
+    function string() {
+        while (peek() !== '"' && !isAtEnd()) {
+            if (peek() !== "\n") line++;
+            advance();
+        }
+
+        if (isAtEnd()) {
+            loxError(line, "Unterminated string.");
+            return;
+        }
+
+        // closing "
+        advance();
+
+        addToken("STRING", curLexeme().slice(1, -1));
+    }
+
+    function match(expected: string): boolean {
+        if (isAtEnd()) return false;
+        if (source[current] !== expected) return false;
+        current++;
+        return true;
+    }
+
+    function peek() {
+        return isAtEnd() ? "\0" : source[current];
+    }
+
+    function peekNext() {
+        if (current + 1 > source.length) return "\0";
+        return source[current + 1];
+    }
+
+    function isAlpha(c: string) {
+        return /^[a-zA-Z_]$/.test(c);
+    }
+
+    function isDigit(c: string) {
+        return /^[0-9]$/.test(c);
+    }
+
+    function isAlphaNumeric(c: string) {
+        return isAlpha(c) || isDigit(c);
+    }
+
+    function advance() {
+        return source[current++];
+    }
+
+    function curLexeme() {
+        return source.slice(start, current);
+    }
+
+    function addToken(type: TokenType, literal?: Token["literal"]) {
+        const lexeme = curLexeme();
+        tokens.push({ type, lexeme, literal, line });
+    }
+
+    function isAtEnd() {
+        return current >= source.length;
+    }
+}
